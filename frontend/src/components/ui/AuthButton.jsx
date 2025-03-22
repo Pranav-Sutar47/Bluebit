7d966910dc68be52c7196da55531b2669e63c23a
Pranav <pranavsutar4747@gmail.com>
2025-03-23 05:08:28 +0530

    Added Components

--- frontend/src/components/ui/AuthButton.jsx
+++ frontend/src/components/ui/AuthButton.jsx
@@ -1,53 +1,107 @@
-import React, { useContext } from 'react';
-import { motion } from 'framer-motion';
+import React, { useContext, useEffect } from "react";
+import { motion } from "framer-motion";
 import { auth, googleProvider, signInWithPopup } from "../../firebase";
-import axios from 'axios'
-import AppContext from '@/context/AppContext';
+import axios from "axios";
+import AppContext from "@/context/AppContext";
+import { useToast } from "@/hooks/use-toast";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useNavigate } from "react-router-dom";
 
 const AuthButton = () => {
+  const { login, setLogin } = useContext(AppContext);
 
-    const {setLogin} = useContext(AppContext);
+  const navigate = useNavigate();
 
-    const handleLogin = async () => {
-      try {
-        const result = await signInWithPopup(auth, googleProvider);
-        const idToken = await result.user.getIdToken();
-  
-        const url = String(import.meta.env.VITE_BASEURL) + "auth/login/";
- 
-        const response = await axios.post(url, { idToken }, {
+  useEffect(() => {
+    if (localStorage.getItem("token") != null) setLogin(true);
+    else setLogin(false);
+  }, [login]);
+
+  const { toast } = useToast();
+
+  const logOut = ()=>{
+      localStorage.clear();
+      setLogin(prev=>!prev);
+      toast({
+        description: "User Logged Out Successfully!",
+        className: "bg-green-500 text-white",
+      });
+  }
+
+  const handleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      const idToken = await result.user.getIdToken();
+
+      const url = String(import.meta.env.VITE_BASEURL) + "auth/login/";
+
+      const response = await axios.post(
+        url,
+        { idToken },
+        {
           headers: { "Content-Type": "application/json" },
-        });
-        
-
-
-        if (response.status === 200) {
-          alert('User is authenticated');
-          setLogin(true);
-          localStorage.setItem('token',response.data.tokens.access);
-        } else {
-          alert('Error',response.data);
-          setLogin(false);
         }
-      } catch (error) {
+      );
+
+      if (response.status === 200) {
+        setLogin(true);
+        localStorage.setItem("token", response.data.tokens.access);
+        toast({
+          description: "User Logged In Successfully!",
+          className: "bg-green-500 text-white",
+        });
+      } else {
+        toast({
+          variant: "destructive",
+          title: "Uh oh! Something went wrong.",
+          description: "There was a problem with your request.",
+        });
         setLogin(false);
-        console.error("Error during login:", error);
       }
-    };
+    } catch (error) {
+      setLogin(false);
+      console.error("Error during login:", error);
+    }
+  };
 
-
-
-  // Otherwise, show login button
-  return (
-    <motion.button
-      className="text-gray-700 font-medium hover:text-blue-600 transition-colors"
-      whileHover={{ scale: 1.05 }}
-      whileTap={{ scale: 0.95 }}
-      onClick={handleLogin}
-    >
-      Login
-    </motion.button>
-  );
+  if (login) {
+    return (
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png"/>
+            <AvatarFallback>PF</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          {/* Add Profile link here */}
+          <DropdownMenuItem onClick={navigate()}>Profile</DropdownMenuItem> 
+          <DropdownMenuItem onClick={logOut}>Log Out</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    );
+  } else {
+    return (
+      <motion.button
+        className="text-gray-700 font-medium hover:text-blue-600 transition-colors"
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+        onClick={handleLogin}
+      >
+        Login
+      </motion.button>
+    );
+  }
 };
 
 export default AuthButton;
